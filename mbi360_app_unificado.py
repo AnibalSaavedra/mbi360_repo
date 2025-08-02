d406c034431dd1bf74f442cc4ae5ccc2dde79fad
AnibalSaavedra <anibal.uv@gmail.com>
2025-08-02 17:08:13 -0400

    Add files via upload

--- mbi360_app_unificado.py
+++ mbi360_app_unificado.py
@@ -69,7 +69,6 @@ def ejecutar_test_condiciones_clinicas():
 # ---- APP PRINCIPAL ----
 
 import streamlit as st
-from modulo_epigenetico import ejecutar_test_epigenetico
 from modulo_condiciones import ejecutar_test_condiciones_clinicas
 
 st.set_page_config(page_title="MBI 360°", page_icon="🌀", layout="centered")
